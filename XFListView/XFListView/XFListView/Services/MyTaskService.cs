57aa0b49ab31b20893443eef0b023dcf8de22aac
李進興 Vulcan <vulcan@miniasp.com>
2018-05-09 12:50:56 +0800

    集合資料CRUD 顯示資料 0

---
+++ XFListView/XFListView/XFListView/Services/MyTaskService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using XFListView.Models;
+
+namespace XFListView.Services
+{
+    public class MyTaskService
+    {
+        public static int LastID = 0;
+        public static List<MyItem> GetAllTask()
+        {
+            List<MyItem> fooMyItems =
+                new List<MyItem>();
+            for (int i = 0; i < 100; i++)
+            {
+                fooMyItems.Add(new MyItem()
+                {
+                    Id = LastID,
+                    Name = $"工作 {LastID}",
+                    Status = (LastID % 3) == 0 ? 
+                    "尚未開始" : "已完成",
+                    Date = DateTime.Now.AddDays(LastID),                    
+                });
+                LastID++;
+            }
+            return fooMyItems;
+        }
+    }
+}
---
+++ XFListView/XFListView/XFListView/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,53 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using XFListView.Models;
+using XFListView.Services;
+
+namespace XFListView.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ObservableCollection<MyItem> MyTasks { get; set; } =
+            new ObservableCollection<MyItem>();
+        public MyItem MyTaskSelected { get; set; }
+        private readonly INavigationService _navigationService;
+
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            Refresh();
+        }
+
+        void Refresh()
+        {
+            var foo = MyTaskService.GetAllTask();
+            foreach (var item in foo)
+            {
+                MyTasks.Add(item);
+            }
+        }
+    }
+}
